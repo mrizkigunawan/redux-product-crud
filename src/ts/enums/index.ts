4c2f8f84d9659ebd5a72e2fdcf41f91bae7bc205
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-09-03 09:21:11 +0800

    add ts enums

---
+++ src/ts/enums/index.ts
@@ -0,0 +1,14 @@
+export enum ActionType {
+  FETCH_PRODUCTS_SUCCESS = "fetch_products_success",
+  FETCH_PRODUCTS_REQUEST = "fetch_product_request",
+  FETCH_PRODUCTS_FAIL = "fetch_products_fail",
+  ADD_PRODUCT_SUCCESS = "add_product_success",
+  SET_PRODUCT_SUCCESS = "set_product_success",
+  SET_PRODUCT_REQUEST = "set_product_request",
+  SET_PRODUCT_FAIL = "set_product_fail",
+  UNSET_PRODUCT = "unset_product",
+  UNSET_SUCCESS = "unset_success",
+  EDIT_PRODUCT = "edit_product",
+  DELETE_PRODUCT = "delete_product",
+  CLEAR_ERRORS = "clear_errors",
+}
