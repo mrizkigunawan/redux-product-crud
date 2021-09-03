29f7ae1dfe7d84ee9047aba4d3d8c6215c1e237e
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-09-03 09:27:09 +0800

    add redux and chakra `provider`

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./state";
+import theme from "./config/theme";
+
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/400.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
