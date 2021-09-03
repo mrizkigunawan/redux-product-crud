ea135cd46fba91f28e3e34f4bfed87a7ec187381
Rizki Gunawan <rizkigunawan300@gmail.com>
2021-09-03 09:31:48 +0800

    add constant file

---
+++ src/constant/index.ts
@@ -0,0 +1,16 @@
+export const BASE_URL = "https://60cb2f6921337e0017e440a0.mockapi.io";
+
+export const PRODUCTS_PER_PAGE = 10;
+
+export const TABLE_HEADINGS = [
+  "NO",
+  "PRODUCT NAME",
+  "QTY",
+  "EXPIRED",
+  "ACTIVE",
+  "ACTION",
+];
+
+export const MAXIMUM_FILE_SIZE = 2_300_000;
+
+export const ALLOWED_IMG_TYPES = ["image/jpeg", "image/jpg", "image/png"];
